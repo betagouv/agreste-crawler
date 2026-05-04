cb456401b0083bd4c7f1a04ca6e835c5c0ff0340
Estelle Comment <estelle.comment@gmail.com>
2026-05-04 17:24:18 +0200

    Add html ids in html blocks for chapeau and complement-titre

--- page_creator/create_blog_entry.py
+++ page_creator/create_blog_entry.py
@@ -503,9 +503,16 @@ def main() -> int:
 
         left_column_content: list[tuple[str, str]] = []
         if complement_titre:
-            left_column_content.append(("text", f'<h2 div="complement-titre">{escape(complement_titre)}</h2>'))
+            left_column_content.append(
+                (
+                    "html",
+                    f'<h2 id="complement-titre">{escape(complement_titre)}</h2>',
+                )
+            )
         if chapeau:
-            left_column_content.append(("text", f'<div id="chapeau">{escape(chapeau)}</div>'))
+            left_column_content.append(
+                ("html", f'<div id="chapeau">{escape(chapeau)}</div>')
+            )
 
         left_width = "8" if right_column_content else "12"
         columns: list[tuple[str, dict[str, object]]] = [
